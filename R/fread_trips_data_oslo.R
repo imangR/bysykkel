9e85e601ef50207854480d2a95839a4439837ff7
ghim <ghayoornia[dot]iman[at]gmail[dot]com>
2019-04-15 21:03:29 +0200

    Create fread_trips_data_oslo.R

---
+++ R/fread_trips_data_oslo.R
@@ -0,0 +1,48 @@
+fread_trips_data_oslo <- function(year, month) {
+
+  if (year >= 2019) {
+
+    base_url <- "http://data.urbansharing.com/oslobysykkel.no/trips/v1"
+    dl_url <- glue::glue("{base_url}/{year}/{sprintf('%0.2d', month)}.csv")
+
+    if (httr::http_error(dl_url)) {
+
+      stop("The download URL is invalid.")
+
+    }
+
+    bike_df <- data.table::fread(input = dl_url, encoding = "UTF-8")
+    bike_df <- tibble::as_tibble(bike_df)
+
+    return(bike_df)
+
+  } else if (year < 2019) {
+
+    base_url <- "https://data-legacy.urbansharing.com/oslobysykkel.no"
+    dl_url <- glue::glue("{base_url}/{year}/{sprintf('%0.2d', month)}.csv.zip")
+
+    if (httr::http_error(dl_url)) {
+
+      stop("The download URL is invalid.")
+
+    }
+
+    # Download trip data to a temporary file, and read it
+    # directly to R
+    #! Problems with utilizing `data.table::fread()` for this purpose,
+    #! which must be solved in a future version of the `bysykkel`package
+    temp <- tempfile()
+    download.file(dl_url, temp)
+    filename <- bysykkel_control_legacy_oslo(year, month)
+    bike_df <- read.csv(unz(temp, filename),
+                        header = TRUE,
+                        stringsAsFactors = FALSE)
+    unlink(temp)
+
+    bike_df <- tibble::as_tibble(bike_df)
+
+    return(bike_df)
+
+  }
+
+}
